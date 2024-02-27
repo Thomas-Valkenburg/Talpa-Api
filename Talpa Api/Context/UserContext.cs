6c3edbc7452f5b149e53599ccc58ea569fe7a5b4
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-02-27 11:46:49 +0100

    Initial setup of project

---
+++ Talpa Api/Context/UserContext.cs
@@ -0,0 +1,10 @@
+﻿using Talpa_Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Talpa_Api.Context
+{
+    public class UserContext(DbContextOptions<UserContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; init; } = null!;
+    }
+}
