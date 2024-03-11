16cc95e0d788eea33d57b63a4311a6d4ef14f350
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-03-11 12:40:44 +0100

    Change MySql to MsSql

--- Talpa Api/Program.cs
+++ Talpa Api/Program.cs
@@ -1,54 +1,49 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace Talpa_Api;
-
-public class Program
+namespace Talpa_Api
 {
-    public static void Main(string[] args)
+    public static class Program
     {
-        var host = CreateHostBuilder(args).Build();
-
-        using (var scope = host.Services.CreateScope())
+        public static void Main(string[] args)
         {
-            var services = scope.ServiceProvider;
+            var builder = WebApplication.CreateBuilder(args);
 
-            try
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var connection = builder.Environment.IsDevelopment()
+                ? "Server=tcp:cgi-the-boys.database.windows.net,1433;Initial Catalog=cgi_the_boys;Persist Security Info=False;User ID=the_boys_admin;Password=Database123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+                : Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+            
+            Console.WriteLine($"connection: {connection}");
+
+            builder.Services.AddDbContext<Context>(options =>
+                options.UseSqlServer(connection));
+            
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
             {
-                var context = services.GetRequiredService<Context>();
-                
-                EnsureDatabaseExists([context]);
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occurred seeding the db.");
-            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
         }
-
-        host.Run();
-    }
-
-    private static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args).ConfigureLogging(logger =>
-        {
-            logger.ClearProviders().AddSimpleConsole(conf =>
-            {
-                conf.TimestampFormat = "[dd/MM/yyyy HH:mm:ss] ";
-            });
-        }).ConfigureWebHostDefaults(webBuilder =>
-        {
-            webBuilder.UseStartup<Startup>();
-        });
-    }
-
-    private static void EnsureDatabaseExists(List<DbContext> contexts)
-    {
-        contexts.ForEach(context =>
-        {
-            context.Database.EnsureCreated();
-
-            context.SaveChanges();
-        });
     }
 }
