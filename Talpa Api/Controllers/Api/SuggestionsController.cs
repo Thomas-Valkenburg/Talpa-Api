4e742933898bf2e293dbdbbd859c7e3d2963e24a
Thomas V <thomasvalkenburg1@gmail.com>
2024-03-19 17:33:54 +0100

    Move and rename ApiController

---
+++ Talpa Api/Controllers/Api/SuggestionsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Talpa_Api.Contexts;
+using Talpa_Api.Models;
+
+namespace Talpa_Api.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuggestionsController(Context context) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> CreateSuggestion(string title, string description, int creatorId)
+        {
+            var user = await context.Users.FindAsync(creatorId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Suggestions.Add(new Suggestion
+            {
+                Title       = title,
+                Description = description,
+                Creator     = user
+            });
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
