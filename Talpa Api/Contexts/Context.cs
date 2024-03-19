07fad130dbb7b7e7093c77f7d0a674d9e580d00f
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-03-19 13:57:05 +0100

    added many to many initializers

--- Talpa Api/Contexts/Context.cs
+++ Talpa Api/Contexts/Context.cs
@@ -21,4 +21,21 @@ public class Context : DbContext
     public virtual DbSet<Poll> Polls { get; set; }
 
     public virtual DbSet<Vote> Votes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Suggestion>()
+            .HasMany(s => s.Tags)
+            .WithMany(t => t.Suggestions);
+
+        modelBuilder.Entity<Suggestion>()
+            .HasMany(s => s.Polls)
+            .WithMany(p => p.Suggestions);
+        
+        modelBuilder.Entity<Team>()
+            .HasOne(t => t.Poll)
+            .WithOne(p => p.Team)
+            .HasForeignKey<Poll>()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
 }
