4f88015b007adc0e1fbcc67502cda821edfa4a3d
Thomas V <36117329+Thomas-Valkenburg@users.noreply.github.com>
2024-03-25 15:38:19 +0100

    New features, moved files, fixed attributes and cleanup. (#20)
    * Cleanup Class Diagram
    
    * Move and rename ApiController
    
    * change set to init
    
    * add required attributes
    
    * Make Creator Optional
    
    * feat: added CalculateSimilarityPercentage method to ApiController (#17)
    
    * feat: added CalculateSimilarityPercentage method to ApiController
    
    * Delete .idea/.idea.Talpa Api/.idea directory
    
    * Move code
    
    ---------
    
    Co-authored-by: Thomas V <36117329+Thomas-Valkenburg@users.noreply.github.com>
    Co-authored-by: Thomas V <thomasvalkenburg1@gmail.com>
    
    * rename creatorid to creator
    
    * Rename enddatetime to enddate
    
    * use var instead of explicit type
    
    * invert if
    
    * remove unnecesary parentheses
    
    * Update .gitignore
    
    ---------
    
    Co-authored-by: Kaan Secen <70695866+KaanSecen@users.noreply.github.com>

--- Talpa Api/Controllers/Api/SuggestionsController.cs
+++ Talpa Api/Controllers/Api/SuggestionsController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Talpa_Api.Contexts;
 using Talpa_Api.Models;
 
@@ -28,6 +28,47 @@ namespace Talpa_Api.Controllers.Api
             await context.SaveChangesAsync();
 
             return NoContent();
+        }
+        
+        private double CalculateSimilarityPercentage(string string1, string string2)
+        {
+            var pairs1 = WordLetterPairs(string1.ToUpper());
+            var pairs2 = WordLetterPairs(string2.ToUpper());
+
+            var intersection = 0;
+            var union        = pairs1.Count + pairs2.Count;
+
+            foreach (var pair1 in pairs1)
+            {
+                for (var number = 0; number < pairs2.Count; number++)
+                {
+                    if (pair1 != pairs2[number]) continue;
+                    
+                    intersection++;
+                    pairs2.RemoveAt(number);
+                    break;
+                }
+            }
+
+            // return the percentage of similarity
+            return 2.0 * intersection * 100 / union;
+        }
+
+        // Required for the CalculateSimilarityPercentage method.
+        private List<string> WordLetterPairs(string str)
+        {
+            var allPairs = new List<string>();
+            var     words    = str.Split(' ');
+
+            foreach (var word in words)
+            {
+                for (var number = 0; number < word.Length - 1; number++)
+                {
+                    allPairs.Add(word.Substring(number, 2));
+                }
+            }
+
+            return allPairs;
         }
     }
 }
