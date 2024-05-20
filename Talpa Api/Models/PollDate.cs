d8876a95847a638f312182ee9809a901288a4147
Thomas V <thomasvalkenburg1@gmail.com>
2024-05-20 16:30:48 +0200

    add PollDate model

---
+++ Talpa Api/Models/PollDate.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Talpa_Api.Models;
+
+public class PollDate
+{
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public int Id { get; set; }
+
+	public required DateTime Date { get; init; }
+
+	public required Poll Poll { get; init; }
+}
