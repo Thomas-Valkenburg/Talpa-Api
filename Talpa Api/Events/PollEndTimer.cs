4bc534c641b3fa66db02941c7796e200d7849f5a
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-05-24 15:12:37 +0200

    Add poll end timer

---
+++ Talpa Api/Events/PollEndTimer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Localization;
+using Talpa_Api.Contexts;
+using Talpa_Api.Controllers.Api;
+using Talpa_Api.Localization;
+using Talpa_Api.Models;
+using Timer = System.Timers.Timer;
+
+namespace Talpa_Api.Events;
+
+public static class PollEndTimer
+{
+	private static readonly List<Timer> Timers = [];
+
+	public static void CreateTimer(Poll poll)
+	{
+		switch (poll)
+		{
+			case { HasEnded: true, HasPointsAssigned: true }:
+				return;
+			case { HasEnded: true, HasPointsAssigned: false }:
+				PollController.AssignPoints(poll.Id);
+				return;
+			default:
+			{
+				var timer = new Timer
+				{
+					Interval = poll.EndDate.Subtract(DateTime.UtcNow).TotalMilliseconds,
+					AutoReset = false,
+				};
+
+				timer.Elapsed += (_, _) => PollController.AssignPoints(poll.Id);
+
+					timer.Start();
+
+				Timers.Add(timer);
+				break;
+			}
+		}
+	}
+}
