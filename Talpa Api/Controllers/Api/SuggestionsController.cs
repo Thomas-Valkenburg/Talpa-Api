79d2fcfa4678ba98dc090aedaa729df8393bfc7e
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-05-24 15:12:24 +0200

    rename x to suggestion

--- Talpa Api/Controllers/Api/SuggestionsController.cs
+++ Talpa Api/Controllers/Api/SuggestionsController.cs
@@ -17,8 +17,8 @@ public class SuggestionsController(Context context, IStringLocalizer<Localizatio
     public async Task<ActionResult<List<Suggestion>>> GetSuggestions()
     {
         return await context.Suggestions
-            .Include(x => x.Creator)
-            .Include(x => x.Tags)
+            .Include(suggestion => suggestion.Creator)
+            .Include(suggestion => suggestion.Tags)
             .ToListAsync();
     }
         
